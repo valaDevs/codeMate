62ca5d6377e91afa48938444321a31f08f3d8dcd
valaDevs <valanerd77@gmail.com>
2021-09-12 18:19:26 +0430

    new version

--- codes.js
+++ codes.js
@@ -6,12 +6,14 @@ let coderImage = document.getElementById('code-avatar')
 let coderName = document.getElementById('code-name')
 let coderCountry = document.getElementById('code-country')
 let coderMail = document.getElementById('code-mail')
+let premium = document.querySelector('.premium')
 
+premium.addEventListener('click' , () => {
+    alert('برای هم کد شدن با محسن مدحج نیاز به حساب ویژه دارید، باتوسعه دهنده هماهنگ کنید')
+})
 const eventHandlers = () => {
-    getUserData()
-    findCoders()
+    getUserData() 
 }
-
 const getUserData = () => {
     if(selfCoder.vlaue !== "" && selfLang.value !== ""){
        let li = document.createElement('li')
@@ -19,14 +21,17 @@ const getUserData = () => {
        youAre.appendChild(li)
        selfCoder.value= ""
         selfLang.value= ""
+     
+        findCoders()
     }
     else{
         alert('مقادیر را مثل آدم بنویس')
+        return false;
     }
 }
 
 const findCoders = () => {
-    fetch('https://randomuser.me/api/?gender=female')
+    fetch('https://randomuser.me/api/')
 .then(response => response.json())
 .then(data => {
     let userData = data.results[0];
@@ -37,13 +42,13 @@ const findCoders = () => {
 })
 
     let fakeName = faker.name.findName();
-    coderName.innerHTML = fakeName
+    coderName.innerHTML = `نام : ${fakeName}`
 
     const fakeCountry = faker.address.country()
-    coderCountry.innerHTML = fakeCountry
+    coderCountry.innerHTML = `کشور : ${fakeCountry}`
 
-    const fakeMail = faker.name.jobTitle()
-    coderMail.innerHTML = fakeMail
+    const fakeMail = faker.phone.phoneNumber()
+    coderMail.innerHTML = `شماره : ${fakeMail}`
 }
 
 
